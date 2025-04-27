85fa82cc072fb1713e4b65794cd5485eca215770
adekoyadapo <49321398+adekoyadapo@users.noreply.github.com>
2025-04-27 14:24:58 -0600

    updating workflow - validating OIDC, comments, fix 403

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,14 @@
 output "pool_name" {
   value = module.gh_oidc.pool_name
+  description = "Pool Name"
 }
 
 output "provider" {
   value = module.gh_oidc.provider_name
+  description = "Provider Name"
 }
 
 output "sa" {
   value = google_service_account.sa.email
+  description = "Service Account Email"
 }
